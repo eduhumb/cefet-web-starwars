7302c72aa663c6a506fb52f12b02630c3a6a3fb8
eduhumb <eduhumb@gmail.com>
2019-05-02 12:24:00 -0300

    get API

--- starwars.js
+++ starwars.js
@@ -3,3 +3,21 @@
 // para fazer uma requisição assíncrona e:
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
+
+if (typeof $ !== 'undefined') {
+
+  // pega o elemento que contém a lista de filmes
+  let $introTextEl = $('nav #movies');
+
+  $.ajax({
+    url: 'https://swapi.co/api/films/',
+    method: 'GET',
+    success: function(result) {
+      console.dir(result)
+      for(m of result.results.sort().reverse()){
+        $('#movies ul').after('<li>' + JSON.stringify(m['title']) + '</li>');
+      }
+    }
+  });
+
+}
